5722c2fbd261629f6a255638540e2afb1aa1c592
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-08 20:34:17 +0000

    Added cli loop

--- knicks_history/lib/knicks_history/cli.rb
+++ knicks_history/lib/knicks_history/cli.rb
@@ -7,8 +7,20 @@ class KnicksHistory::CLI
   end
 
   def start
-    puts "\nWhich season would you like to see stats for? (Enter a year between 1946-2017)"
-    input = gets.strip.to_i
+    input = ""
+    while true
+      puts "\nWhich season would you like to see stats for? (Enter a year between 1946-2017) or type exit."
+      input = gets.strip.to_i
+      if input < 2018 && input > 1945
+        output_stats(input)
+      elsif input == exit
+        puts "Thanks for using my gem! Go Knicks!"
+        break
+      end
+  end
+
+  def output_stats(year)
+    #prints stats for that year
   end
 
 end

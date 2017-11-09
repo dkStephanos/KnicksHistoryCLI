e1961642f2f45a4d894955b4ee54aebf7e52fea3
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-09 19:42:12 +0000

    Adding sub menu

--- knicks_history/lib/knicks_history/cli.rb
+++ knicks_history/lib/knicks_history/cli.rb
@@ -10,11 +10,20 @@ class KnicksHistory::CLI
   def start
     input = ""
     while true
-      puts "\nWhich season would you like to see stats for? (Enter a year between 1946-2017) or type exit."
+      puts "\nWhich season would you like to see stats for? (Enter a year between 1946-2017) or type all to list all seasons or type exit."
       input = gets.strip
       year = input.to_i
       if year < 2018 && year > 1945
         output_stats(input)
+        puts "\nOther Stats (enter the number of the statistic you want to see)\n1. Offensive Rating\n2. Defensive Rating\n3. Best Player w/ Win Shares"
+        input = gets.strip
+        case input
+        when "1"
+        when "2"
+        when "3"
+        else
+          puts "\nSorry, must enter a number 1-3"
+        end
       elsif input == "exit"
         puts "\n\nThanks for using my gem! Go Knicks!"
         break
@@ -27,9 +36,9 @@ class KnicksHistory::CLI
   def output_stats(year)
     #prints stats for that year
     season = KnicksHistory::Season.find_season(year)
-    puts "\n\nYear  Wins  Losses  Win%  Off Rtng  Def Rtng  Best Player(WS)"
+    puts "\n\nYear  Wins  Losses  Win%" #  Off Rtng  Def Rtng  Best Player(WS)"
     #binding.pry
-    puts "#{season[1].year}   #{season[1].wins}     #{season[1].losses}    #{season[1].win_percentage}   #{season[1].off_rating}     #{season[1].def_rating}    #{season[1].best_player_ws}"
+    puts "#{season[1].year}   #{season[1].wins}     #{season[1].losses}    #{season[1].win_percentage}" #{}"   #{season[1].off_rating}     #{season[1].def_rating}    #{season[1].best_player_ws}"
   end
 
 end
